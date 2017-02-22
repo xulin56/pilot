8bf0884085bfc87d6fa90cad7a0268f82d1fe040
jiajie <jiajie.zhang@transwarp.io>
2017-02-22 17:51:13 +0800

    add dependent python pkgs: mysqlclient, jpype, pyodbc

--- setup.py
+++ setup.py
@@ -42,6 +42,9 @@ setup(
     zip_safe=False,
     scripts=['superset/bin/superset'],
     install_requires=[
+        'mysqlclient==1.3.8',
+        'pyodbc==4.0.11',
+        'JPype1==0.6.2',
         'celery==3.1.23',
         'cryptography==1.5.3',
         'flask-appbuilder==1.8.1',
@@ -61,7 +64,7 @@ setup(
         'requests==2.10.0',
         'simplejson==3.8.2',
         'six==1.10.0',
-        'sqlalchemy==1.0.13',
+        'sqlalchemy==1.1.5dev',
         'sqlalchemy-utils==0.32.7',
         'sqlparse==0.1.19',
         'thrift>=0.9.3',
