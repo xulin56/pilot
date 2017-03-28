fd6bccfa48495461120f8998af8c08e6ff56701f
jiajie <jiajie.zhang@transwarp.io>
2017-03-28 19:11:27 +0800

    modify the default config for 'sqlalchemy_url' to sqlite

--- superset/config.py
+++ superset/config.py
@@ -43,11 +43,11 @@ CUSTOM_SECURITY_MANAGER = None
 SECRET_KEY = '=== Transwarp Studio Inceptor ==='  # noqa
 
 # The SQLAlchemy connection string.
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DATA_DIR, 'superset.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DATA_DIR, 'superset.db')
 # If use mysql, the database should be existed and change its charset to 'utf8':
 # 'alter database superset character set utf8'
 # and  'charset=utf8' should be in uri
-SQLALCHEMY_DATABASE_URI = 'mysql://superset:123456@172.16.1.85:3306/superset?charset=utf8'
+#SQLALCHEMY_DATABASE_URI = 'mysql://username:password@lcoalhost:3306/database?charset=utf8'
 
 # The limit of queries fetched for query search
 QUERY_SEARCH_LIMIT = 1000
