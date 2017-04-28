4b8d682faee23155f4340207ff8fa4218214fdcc
xl <1132777020@qq.com>
2017-04-28 11:29:23 +0800

    chongtu

--- superset/assets/javascripts/list.js
+++ superset/assets/javascripts/list.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-
 import { operate } from './components/Slice';
 
 
