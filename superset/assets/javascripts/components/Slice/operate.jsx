54c1fc12513a9c167da80730eb523adad479e699
rockGithub <rock20110414@gmail.com>
2017-04-27 20:34:37 -0700

    reasonable changes: adding api of getting slicemodelview/listJson

--- superset/assets/javascripts/components/Slice/operate.jsx
+++ superset/assets/javascripts/components/Slice/operate.jsx
@@ -4,9 +4,10 @@ import { render } from 'react-dom';
 const $ = window.$ = require('jquery');
 
 $(function(){
-    let url = '/slicemodelview/listJson';
+    let url = '/slicemodelview/listJson/';
 
     $.getJSON(url, function (data) {
+        const store = window.localStorage;
         console.log( 'data is:', data);
     });
 
