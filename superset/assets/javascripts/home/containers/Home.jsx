6703630872427a4846be3c07a859135cfe14f46e
yizhen.fan <yizhen.fan@transwarp.io>
2017-05-19 20:13:32 +0800

    WARP-14455: add page structrue

---
+++ superset/assets/javascripts/home/containers/Home.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { DataTendency, FavouritePanel, EditAndEvent} from './';
+import PropTypes from 'prop-types';
+import { fetchPosts } from "../actions";
+import { connect } from 'react-redux';
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        const { dispatch } = this.props;
+        dispatch(fetchPosts());
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log(nextProps.param);
+    }
+
+    render() {
+
+        const { param, state } = this.props;
+
+        return (
+          <div>
+                <DataTendency></DataTendency>
+                <FavouritePanel></FavouritePanel>
+                <EditAndEvent></EditAndEvent>
+          </div>
+        );
+    }
+}
+
+Home.propTypes = {
+    param: PropTypes.any.isRequired,
+    isFetching: PropTypes.any.isRequired,
+    lastUpdated: PropTypes.any.isRequired
+}
+
+const mapStateToProps = (state, props) => {
+    const { posts } = state;
+    return {
+        isFetching: posts.isFetching,
+        param: posts.param,
+        lastUpdated: posts.lastUpdated || false
+    };
+}
+
+export default connect(mapStateToProps)(Home);
